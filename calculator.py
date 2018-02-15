88264d2d8a1ff3f51c9b1a387dee2ecb62239938
jeff mutahi <jeffmnjogu@gmail.com>
2018-02-15 04:47:28 +0300

    enter

---
+++ calculator.py
@@ -0,0 +1,10 @@
+class Calculator(object):
+
+	def add(self, x, y):
+		number_types = (int, float, complex)
+
+		if isinstance(x, number_types) and isinstance(y, number_types):
+			import pdb; pdb.set_trace()
+			return x+y
+		else:
+			raise ValueError
